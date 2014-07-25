a984864a0ae898d44fd5195943c4f241c422860b
Michael Gordon <gordon@astro.umn.edu>
2014-07-25 11:05:21 -0500

    Added query_mod

---
+++ utils/query_mod.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+######################
+def query(name):
+    '''
+    INPUT
+    name = object string to query
+
+    OUTPUT
+    RA  = string of form: 'hh:mm:ss.ss'
+    DEC = string of form: '[+/-]dd:mm:ss.ss'
+    '''
+    pass
+######################
